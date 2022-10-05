820f5e5582a1e62de2d23dee9c642cb6b253a551
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-10-05 21:33:28 +0100

    Update ppp.c

--- ppp.c
+++ ppp.c
@@ -1,64 +1,60 @@
 #include "monty.h"
 /**
- * push - push a node onto the stack
+ * pop - removes top item from stack
  * @stack: stack
- * @line_number: line number in monty .m file
+ * @line_number: monty file line number
  * Return: void
  */
-void push(stack_t **stack, unsigned int line_number
-	__attribute__((unused)))
-{
-	stack_t *tmp;
-
-	tmp = malloc(sizeof(stack_t)); /* not freed */
-	if (tmp == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed");
-		exit_free(*stack);
-		exit(EXIT_FAILURE);
-	}
-	tmp->n = globes.data;
-	tmp->prev = NULL;
-	tmp->next = NULL;
-	if (*stack != NULL)
-	{
-		tmp->next = *stack;
-		(*stack)->prev = tmp;
-	}
-	*stack = tmp;
-}
-/**
- * pall - print all data in stack
- * @stack: stack
- * @line_number: line number in monty .m file
- * Return: void
- */
-void pall(stack_t **stack, unsigned int line_number)
+void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
 
+	if (tmp == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+	if (tmp->next == NULL)
+	{
+		free(*stack);
+		*stack = NULL;
+	}
+	else
+	{
+		(*stack) = (*stack)->next;
+		(*stack)->prev = NULL;
+		free(tmp);
+	}
+}
+/**
+ * nop - do nothing
+ * @stack: unused
+ * @line_number: unused
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
 	(void) line_number;
-	while (tmp != NULL)
-	{
-		printf("%d\n", tmp->n);
-		tmp = tmp->next;
-	}
 }
 /**
- * pint - print top int
+ * swap - swaps data from top two nodes in stack
  * @stack: stack
- * @line_number: line number in monty .m file
+ * @line_number: monty file line number
  * Return: void
  */
-void pint(stack_t **stack, unsigned int line_number)
+void swap(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack;
+	int temp;
 
-	if (tmp == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit_free(*stack);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", tmp->n);
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
